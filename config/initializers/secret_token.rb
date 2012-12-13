6edda421704dacef4e41179bd58dcf94fa4b370c
Jonathan Lee <jlee@ctgla.org>
2012-12-13 13:05:27 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapercal::Application.config.secret_token = 'a68a0f409ed8c7be523cea10354e666a1d55f07d3c2652f5cabc389933d1fba1ab3d0f4d62bf258c6a6b76935312173569f25376e01d9832814b467744ab156d'
